48a5ed03c2c75a0f4a19cddab56422e318ddadc1
Cheng <cheng.shi@unibas.ch>
2021-05-04 22:11:16 +0200

    96.17 auc

--- perturbation_geometric.py
+++ perturbation_geometric.py
@@ -9,15 +9,14 @@ from torch.nn import Linear
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 from torch_geometric.nn import global_mean_pool
-
-batch_size = 100
-GCN_hidden1 = 16;
-GCN_hidden2 = 16;
-GCN_hidden3 = 16;
+GCN_hidden1 = 128;
+GCN_hidden2 = 64;
+GCN_hidden3 = 32;
+GCN_hidden4 = 16;
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
-print(torch.cuda.get_device_name(0))
+#print(torch.cuda.get_device_name(0))
 
 def non_ptb_data(index, n, x, y):
     # prepare the non-perturbed data
@@ -61,15 +60,20 @@ def ptb_data_test(index, n, x, label):
     return data
 
 
+
+
+
 class MLP(torch.nn.Module):
     def __init__(self,input_size):
         super(MLP, self).__init__()
-        self.linear1 = torch.nn.Linear(input_size,2000)
-        self.linear2 = torch.nn.Linear(2000,1000)
-        self.linear3 = torch.nn.Linear(1000,800)
-        self.linear4 = torch.nn.Linear(800,200)
-        self.linear5 = torch.nn.Linear(200,1)
+        self.linear1 = torch.nn.Linear(input_size,200)
+        self.linear2 = torch.nn.Linear(200,100)
+        self.linear3 = torch.nn.Linear(100,80)
+        self.linear4 = torch.nn.Linear(80,20)
+        self.linear5 = torch.nn.Linear(20,1)
         self.act1= nn.ReLU()
+        #self.act2= nn.ReLU()
+        #self.act3= nn.ReLU()
         self.act2= torch.sin
         self.act3= torch.sin
         self.act4= nn.ReLU()
@@ -94,31 +98,43 @@ class Net(torch.nn.Module):
         self.conv1 = GCNConv(non_perturb_train.num_node_features, GCN_hidden1)
         self.conv2 = GCNConv(GCN_hidden1, GCN_hidden2)
         self.conv3 = GCNConv(GCN_hidden2, GCN_hidden3)
-        self.mlp = MLP((GCN_hidden1+GCN_hidden2+GCN_hidden3)*n)
+        self.conv4 = GCNConv(GCN_hidden3, GCN_hidden4)
+        self.mlp = MLP((GCN_hidden1+GCN_hidden2+GCN_hidden3+GCN_hidden4)*n)
 
     def forward(self, x_p, x_np, y, edge_index_p,edge_index_np):
         o_p_1 = self.conv1(x_p, edge_index_p)
         o_p_1 = o_p_1.relu()
-        o_p_1 = F.dropout(o_p_1, p=0.5, training=self.training)
+        #o_p_1 = torch.sin(o_p_1)
+        #o_p_1 = F.dropout(o_p_1, p=0.1, training=self.training)
     
         o_p_2 = self.conv2(o_p_1, edge_index_p)
         o_p_2 = o_p_2.relu()
-        o_p_2 = F.dropout(o_p_2, p=0.5, training=self.training)
+        #o_p_2 = torch.sin(o_p_2)
+        #o_p_2 = F.dropout(o_p_2, p=0.5, training=self.training)
 
         o_p_3 = self.conv3(o_p_2, edge_index_p)
+        o_p_3 = o_p_3.relu()
+
+        o_p_4 = self.conv4(o_p_3, edge_index_p)
        
         o_np_1 = self.conv1(x_np, edge_index_np)
+        #o_np_1 = torch.sin(o_np_1)
         o_np_1 = o_np_1.relu()
-        o_np_1 = F.dropout(o_np_1, p=0.5, training=self.training)
+        #o_np_1 = F.dropout(o_np_1, p=0.5, training=self.training)
     
         o_np_2 = self.conv2(o_np_1, edge_index_np)
         o_np_2 = o_np_2.relu()
-        o_np_2 = F.dropout(o_np_2, p=0.5, training=self.training)
+        #o_np_2 = torch.sin(o_np_2)
+        #o_np_2 = F.dropout(o_np_2, p=0.5, training=self.training)
 
         o_np_3 = self.conv3(o_np_2, edge_index_np)
+        o_np_3 = o_np_3.relu()
 
-        o_p = torch.cat((o_p_1,o_p_2,o_p_3),dim= 1)
-        o_np = torch.cat((o_np_1,o_np_2,o_np_3),dim= 1)
+        o_np_4 = self.conv4(o_np_3, edge_index_np)
+
+
+        o_p = torch.cat((o_p_1,o_p_2,o_p_3,o_p_4),dim= 1)
+        o_np = torch.cat((o_np_1,o_np_2,o_np_3,o_np_4),dim= 1)
         
         factor = y.size(0)
         z = o_np.repeat(factor,1)
@@ -131,57 +147,66 @@ class Net(torch.nn.Module):
 
         return z
 
-fname = 'USAir'
-n = 332
-x = torch.eye(n,dtype=torch.float)
-train_data = pd.read_csv(fname+'_train_el.csv',header=None)
-train_edge_index = np.array(train_data.iloc[:,0:])
-non_perturb_train = non_ptb_data(train_edge_index,n,x,np.zeros((1,2))).to(device)
 
-train_labels = pd.read_csv(fname+'_train_labels.csv',header=None)
-train_labels = np.array(train_labels.iloc[:,0:])
-test_labels = pd.read_csv(fname+'_test_labels.csv',header=None)
-test_labels = np.array(test_labels.iloc[:,0:])
+if __name__ == '__main__':
+    fname = 'USAir'
+    n = 332
+    batch_size = 64
+    x = torch.eye(n,dtype=torch.float)
+    #x = torch.ones((n,1))
+    train_data = pd.read_csv(fname+'_train_el.csv',header=None)
+    train_edge_index = np.array(train_data.iloc[:,0:])
+    non_perturb_train = non_ptb_data(train_edge_index,n,x,np.zeros((1,2))).to(device)
 
-train_data_list = [ptb_data_train(train_edge_index,n,x,train_labels[i,0:3]) for i in range(0,len(train_labels))]
-train_loader = DataLoader(train_data_list,batch_size=batch_size,shuffle=True)
-test_data_list = [ptb_data_test(train_edge_index,n,x,test_labels[i,0:3]) for i in range(0,len(test_labels))]
-test_loader = DataLoader(test_data_list,batch_size=len(test_data_list),shuffle=False)
+    train_labels = pd.read_csv(fname+'_train_labels.csv',header=None)
+    train_labels = np.array(train_labels.iloc[:,0:])
+    test_labels = pd.read_csv(fname+'_test_labels.csv',header=None)
+    test_labels = np.array(test_labels.iloc[:,0:])
 
-model = Net(hidden_channels=12).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.000001)
-criterion = torch.nn.BCEWithLogitsLoss()
+    train_data_list = [ptb_data_train(train_edge_index,n,x,train_labels[i,0:3]) for i in range(0,len(train_labels))]
+    train_loader = DataLoader(train_data_list,batch_size=batch_size,shuffle=True)
+    test_data_list = [ptb_data_test(train_edge_index,n,x,test_labels[i,0:3]) for i in range(0,len(test_labels))]
+    test_loader = DataLoader(test_data_list,batch_size=int(len(test_data_list)),shuffle=True)
 
-def train():
-    model.train()
+    model = Net(hidden_channels=12).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    #criterion = torch.nn.BCEWithLogitsLoss()
+    criterion = torch.nn.BCELoss()
+    def train(train_loader):
+        model.train()
+        loss_eposch=0
+        for step, data in enumerate(train_loader):# Iterate in batches over the training dataset.
+            data = data.to(device)
+            out = model(data.x, non_perturb_train.x, data.y, data.edge_index, non_perturb_train.edge_index) 
+            out = out.reshape(-1).to(device) # Perform a single forward pass.
+            #print('aa',out)
+            #print('bb',data.y[:,0])
+            loss = criterion(out, data.y[:,0])  # Compute the loss.
+            loss.backward()  # Derive gradients.
+            optimizer.step()  # Update parameters based on gradients.
+            optimizer.zero_grad()  # Clear gradients.
+            #print(f"Step:{step},Loss:{loss.item()}")
+            loss_eposch=loss_eposch+loss.item()
+        return loss_eposch/(step+1.0)
 
-    for data in train_loader:  # Iterate in batches over the training dataset.
-         data = data.to(device)
-         out = model(data.x, non_perturb_train.x, data.y, data.edge_index, non_perturb_train.edge_index) 
-         out = out.reshape(-1).to(device) # Perform a single forward pass.
-         loss = criterion(out, data.y[:,0])  # Compute the loss.
-         loss.backward()  # Derive gradients.
-         optimizer.step()  # Update parameters based on gradients.
-         optimizer.zero_grad()  # Clear gradients.
-         return loss.item()
+    def test(loader):
+         model.eval()
 
-def test(loader):
-     model.eval()
+         correct = 0
+         for step, data in enumerate(loader):  # Iterate in batches over the training/test dataset.
+             #print(f'Num of graphs in the current batch: {data.num_graphs}')
+             data = data.to(device)
+             out = model(data.x, non_perturb_train.x, data.y, data.edge_index, non_perturb_train.edge_index) 
+             scores = out.cpu().detach().numpy()
+             #print(scores)
+             labels = data.y[:,1].cpu().detach().numpy()
+             break
+         return roc_auc_score(labels, scores)  # Derive ratio of correct predictions.
 
-     correct = 0
-     for step, data in enumerate(loader):  # Iterate in batches over the training/test dataset.
-         #print(f'Num of graphs in the current batch: {data.num_graphs}')
-         data = data.to(device)
-         out = model(data.x, non_perturb_train.x, data.y, data.edge_index, non_perturb_train.edge_index) 
-         scores = out.cpu().detach().numpy()
-         #print(scores)
-         labels = data.y[:,1].cpu().detach().numpy()
-     return roc_auc_score(labels, scores)  # Derive ratio of correct predictions.
-
-num_epochs = 200
-for epoch in range(0, num_epochs):
-    loss_step = train()
-    train_acc = test(train_loader)
-    test_acc = test(test_loader)
-    print('Epoch [{}/{}], Loss: {:.4f}, Train AUC: {:.4f}, Test AUC: {:.4f}'\
-                .format(epoch+1, num_epochs, loss_step, train_acc, test_acc))
+    num_epochs = 20
+    for epoch in range(0, num_epochs):
+        loss_step = train(train_loader)
+        train_acc = test(train_loader)
+        test_acc = test(test_loader)
+        print('Epoch [{}/{}], Loss: {:.4f}, Train AUC: {:.4f}, Test AUC: {:.4f}'\
+                    .format(epoch+1, num_epochs, loss_step, train_acc, test_acc))
